0397152864ecf295d6e7ca3e76380f13b7de5ca4
Ottogod <alexandre.kha@yahoo.com>
2023-02-24 02:39:04 +0000

    concur pb fixed

--- nlu_test/actions/actions.py
+++ nlu_test/actions/actions.py
@@ -32,6 +32,10 @@ current_answer2 = "bonjours"
 current_answers = ["std", "bonjours"]
 turn = 0
 
+accepted_answers_1 = []
+accepted_answers_2 = []
+accepted_answers = {0:[], 1:[]}
+
 nb_question = len(list(questions.keys()))
 
 
@@ -57,6 +61,7 @@ class ActionAskQuestion(Action):
 		dispatcher.utter_message(text = response)
 		return []
 
+
 class ActionUpdateAnswer(Action):
 
 	
@@ -75,9 +80,9 @@ class ActionUpdateAnswer(Action):
 		dispatcher.utter_message(text = "Turn {}. Player 1 says : {}. What do you think Player 2 ?".format(turn, current_answers[0]))
 
 		return [SlotSet("answer1",None)]
-			
 
-		
+
+	
 
 class ActionCheckAnswer(Action):
 
@@ -90,7 +95,7 @@ class ActionCheckAnswer(Action):
 			tracker: Tracker, 
 			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-		global current_answers, turn, questions
+		global accepted_answers_1, accepted_answers_2, turn, questions
 			
 		correct_answer = list(questions.values())[0]
 
@@ -115,3 +120,72 @@ class ActionCheckAnswer(Action):
 		dispatcher.utter_message(text = response)
 
 		return [SlotSet("answer1", None)]
+
+"""			
+class ActionUpdateAnswer(Action):
+
+	def name(self) -> Text:
+			return "action_update_answer"
+
+	def run(self, 
+			dispatcher: CollectingDispatcher, 
+			tracker: Tracker, 
+			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+		global  accepted_answers, turn
+
+		for entity in tracker.latest_message["entities"]:
+			if entity["entity"] == "answer" and entity["role"] == "accepted":
+				accepted_answers[0].append(entity["value"])
+		dispatcher.utter_message(text = "Turn {}. Player 1 says : {}. What do you think Player 2 ?".format(turn, accepted_answers[0]))
+
+		return []
+"""
+"""
+class ActionCheckAnswer(Action):
+
+
+	def name(self) -> Text:
+			return "action_check_answer"
+
+	def run(self, 
+			dispatcher: CollectingDispatcher, 
+			tracker: Tracker, 
+			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+		global accepted_answers, turn, questions
+			
+		correct_answer = list(questions.values())[0]
+
+		turn += 1
+		mod_turn = turn%2 
+
+		answers = []
+		for entity in tracker.latest_message["entities"]:
+			if entity["entity"] == "answer" and entity["role"] == "accepted":
+				accepted_answers[mod_turn].append(entity["value"])
+			elif entity["entity"] == "answer" and entity["role"] == "denied":
+				if entity["value"] in accepted_answers[(mod_turn+1)%2]:
+					accepted_answers[(mod_turn+1)%2].pop(accepted_answers[(mod_turn+1)%2].index(entity["value"]))
+
+		if not accepted_answers[mod_turn]:
+			accepted_answers[mod_turn] = accepted_answers[(mod_turn + 1)%2]
+
+		for answer in accepted_answers[0]:
+			if answer in accepted_answers[1]:
+				if answer.lower() == correct_answer.lower():
+					response = "Turn {}. Correct! The answer is indeed {}.".format(turn, correct_answer)
+				else:
+					response = "Turn {}. Incorrect, it is not {}. The answer is : {}.".format(turn, answer, correct_answer)
+				turn = 0
+
+				dispatcher.utter_message(text = response)
+				return []
+
+		response = "Turn {}. Player 1 says {}, Player 2 says {}. You don't seem to agree. What do you think Player {} ?".format(turn, accepted_answers[0], accepted_answers[1], (mod_turn + 1)%2 + 1)
+		
+
+		dispatcher.utter_message(text = response)
+
+		return []
+"""
