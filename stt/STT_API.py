3cd7cb9fbb067dd5c4083976662ca552955342fc
Levinin <andrewsedmondson@yahoo.co.uk>
2023-03-21 23:06:54 +0000

    Implement muting using data chunks.

--- stt/STT_API.py
+++ stt/STT_API.py
@@ -3,6 +3,7 @@ from threading import Thread
 import STT
 import json
 import logging
+from queue import Queue
 
 __hostname = "127.0.0.1"
 __tts_port = 5001
@@ -15,18 +16,22 @@ app = Flask(__name__)
 transcribe = STT.Transcribe()
 mute_flag = False
 
+
 # Get request must return sentence from STT_Module queue
 @app.route('/incoming_speech',methods=['GET'])
 def return_speech():
     contents = transcribe.get_from_queue()
     if mute_flag is True:
-        # print(f"Muted, not sending data back. {contents}")
-        return {"text": "None", "player": "None"}, 204
+        if not contents is None:       
+            print(f"Muted, not sending data back. {contents}")
+        contents = {"text": "None", "player": "None"}, 204
     elif contents is None:
-        return {"text": "None", "player": "None"}, 204
+        contents = {"text": "None", "player": "None"}, 204
     else:
         print(f"Open line, sending back: {contents}")
-        return contents
+    
+    return contents
+
     
 @app.route('/mute',methods=['POST'])
 def mute():
@@ -36,11 +41,13 @@ def mute():
     global mute_flag
     if data['mute'] == 'mute':
         print("Muting the microphone.")
-        mute_flag = True
+        transcribe.mute_mic(0)
     if data['mute'] == 'unmute':
         print("Unmuting the microphone.")
         mute_flag = False
+        transcribe.mute_mic(1)
     return {"code":200}
+
 
 def basic_queue_testing():
     queue_contents = transcribe.get_from_queue()
@@ -53,23 +60,7 @@ def run_trans(obj):
 
 if __name__ == "__main__":
     logging.getLogger('werkzeug').setLevel(logging.CRITICAL)
+
     trans_thread = Thread(target=run_trans, args=(transcribe,))
     trans_thread.start()
     app.run(debug=True, port=__tts_port, host=__hostname)
-
-
-# Defunct Testing code
-
-# if __name__ == "__main__":
-#     user_choice = input("1: Run Sever 2: Test Queue Pulling\n")
-#     if user_choice == '2':
-#         trans_thread = Thread(target=run_trans, args=(transcribe,))
-#         trans_thread.start()
-#         while True:
-#             user_input = input("Any key to get")
-#             basic_queue_testing()
-#     else:
-#         # Start STT thread for constant listening and appending to dialogue queue
-#         trans_thread = Thread(target=transcribe.start_trans)
-#         trans_thread.start()
-#         app.run(debug=True, port=__tts_port, host=__hostname)
