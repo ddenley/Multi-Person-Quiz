e6f6a3bc8c4d247e95d27c716e104b67ec0a32aa
Levinin <andrewsedmondson@yahoo.co.uk>
2023-03-21 05:37:01 +0000

    Changes to enable mute when talking.

--- stt/STT_API.py
+++ stt/STT_API.py
@@ -1,27 +1,46 @@
-from flask import Flask
-from flask_api import status
+from flask import Flask, request
 from threading import Thread
 import STT
-import requests
+import json
+import logging
 
 __hostname = "127.0.0.1"
 __tts_port = 5001
+
+
 
 app = Flask(__name__)
 
 # Transcription object - will contain the queue to pull from
 transcribe = STT.Transcribe()
-
+mute_flag = False
 
 # Get request must return sentence from STT_Module queue
-@app.get("/incoming_speech")
+@app.route('/incoming_speech',methods=['GET'])
 def return_speech():
     contents = transcribe.get_from_queue()
-    if contents is None:
-        return {"text": "None", "player": "None"}, status.HTTP_204_NO_CONTENT
+    if mute_flag is True:
+        # print(f"Muted, not sending data back. {contents}")
+        return {"text": "None", "player": "None"}, 204
+    elif contents is None:
+        return {"text": "None", "player": "None"}, 204
     else:
+        print(f"Open line, sending back: {contents}")
         return contents
-
+    
+@app.route('/mute',methods=['POST'])
+def mute():
+    # jsondata = request.get_json()
+    # data = json.loads(jsondata)
+    data = request.get_json()
+    global mute_flag
+    if data['mute'] == 'mute':
+        print("Muting the microphone.")
+        mute_flag = True
+    if data['mute'] == 'unmute':
+        print("Unmuting the microphone.")
+        mute_flag = False
+    return {"code":200}
 
 def basic_queue_testing():
     queue_contents = transcribe.get_from_queue()
@@ -33,6 +52,7 @@ def run_trans(obj):
 
 
 if __name__ == "__main__":
+    logging.getLogger('werkzeug').setLevel(logging.CRITICAL)
     trans_thread = Thread(target=run_trans, args=(transcribe,))
     trans_thread.start()
     app.run(debug=True, port=__tts_port, host=__hostname)
