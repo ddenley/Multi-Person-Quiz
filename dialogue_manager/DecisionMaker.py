89e0b5808a79b4e59694b45665d8894bcd4f6b44
rapha <raphael.valeri@ensta-bretagne.org>
2023-03-25 16:39:42 +0000

    Fix the problem with repeat_question

--- dialogue_manager/DecisionMaker.py
+++ dialogue_manager/DecisionMaker.py
@@ -42,7 +42,10 @@ class DecisionMaker:
         msg = ''
         if self.previousMsg is not None:
             if currentUtt[0] == 'repeat_question':
-                msg = self.__Action.sendTTS(self.previousMsg)
+                if self.previousMsg == '':
+                    msg = self.__Action.sendTTS(self.previousMsg)
+                else:
+                    msg = self.__Action.repeatQuestion()
         # if no questions have been asked or if the bot proposed to skip one:
         if (not self.__questionAsked) or (previousAct in ['proposeSkipQ', 'confirm_ans']):
             # If just one player answers when the bot asked if they want to play
@@ -108,20 +111,22 @@ class DecisionMaker:
                             msg = self.checkAgreement()
 
             elif currentUtt[0] == 'agree' and (previousAct != 'proposeClue'):
-                if (self.__currentAnswer is not None) and (self.__currentAnswer.casefold() in [x.casefold() for x in self.__Action.getQManager().getMultipleChoices()]):
-                    p = random.random()
-                    if p < self.pRandom:
-                        msg = self.__Action.confirm(ans=self.__currentAnswer)
+                if self.__currentAnswer is not None:
+                    if self.multipleChoices and (self.__currentAnswer.casefold() not in [x.casefold() for x in self.__Action.getQManager().getMultipleChoices()]):
+                        pass
                     else:
-                        msg = self.__Action.checkAnswer(self.__currentAnswer)
-                        # Reset the current answer of each person to None
-                        self.__currentAnswer0 = None
-                        self.__currentAnswer1 = None
-                        # Reset the boolean
-                        self.__questionAsked = False
-                        self.countAnswers = 0
+                        p = random.random()
+                        if p < self.pRandom:
+                            msg = self.__Action.confirm(ans=self.__currentAnswer)
+                        else:
+                            msg = self.__Action.checkAnswer(self.__currentAnswer)
+                            # Reset the current answer of each person to None
+                            self.__currentAnswer0 = None
+                            self.__currentAnswer1 = None
+                            # Reset the boolean
+                            self.__questionAsked = False
+                            self.countAnswers = 0
                 #TODO - Else : Do we need to ask them another country or just wait ?
-
 
             elif (currentUtt[0] == 'disagree') and (previousAct != 'proposeClue'):
                 self.nbDisagree += 1
